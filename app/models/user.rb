d0ca987a7279f6d7817c273f926fae40b1ad813b
Brian Boylen <bbbrianboylen@gmail.com>
2020-08-01 11:44:02 -0500

    Added friend request model and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,9 @@ class User < ApplicationRecord
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
   has_many :inverse_friends, through: :inverse_friendships, source: :user
   
+  has_many :friend_requests_as_receiver, class_name: "FriendRequest", foreign_key: "receiver_id"
+  has_many :friend_requests_as_requestor, class_name: "FriendRequest", foreign_key: "requestor_id"
+
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
