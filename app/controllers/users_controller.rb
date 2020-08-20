25d5130c7e6455586350ac283e5787f7c9f4719b
Brian Boylen <bbbrianboylen@gmail.com>
2020-08-19 20:04:52 -0500

    updated friend request functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,23 @@ class UsersController < ApplicationController
   def index
     @user = current_user
     @user_friends = @user.friends + @user.inverse_friends
+
+    @pending_requests_receiver_ids = []
+    @user.friend_requests_as_requestor.each do |request|
+      @pending_requests_receiver_ids << request.receiver_id
+    end
+
+    @pending_requests_requestor_ids = []
+    @user.friend_requests_as_receiver.each do |request|
+      @pending_requests_requestor_ids << request.requestor_id
+    end
+
     @user_not_friends = User.all # Think of a more efficient way to do this?
-    @user_not_friends = @user_not_friends.reject {|user| @user_friends.include?(user)}
+    @user_not_friends = @user_not_friends.reject {|user| @user_friends.include?(user) || @pending_requests_requestor_ids.include?(user.id)}
     @user_not_friends.delete(@user)
 
     @friend_request = FriendRequest.new
     @friendship = Friendship.new
-
-    @pending_requests_user_ids = []
-    @user.friend_requests_as_requestor.each do |request|
-      @pending_requests_user_ids << request.receiver_id
-    end
   end
 
   def show
